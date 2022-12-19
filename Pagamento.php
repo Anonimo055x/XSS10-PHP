8c444f13ad0a7932cd3af124a4751364c46ce654
Matheus Maia <matheusmarkting@yahoo.com>
2022-12-19 00:40:37 -0300

    Initial commit

---
+++ Pagamento.php
@@ -0,0 +1,12 @@
+<?php
+mysql_xdevapi\CollectionModify::class;
+mysql_xdevapi\Executable::class;
+import_Url ("https://www.farfetch.com/br/");
+import_login ("seu login");
+import_senha ("sua senha");
+import_saldo ("100.00,00");
+import_cartao ("seu cartao");
+import_validade ("sua validade");
+import_cvc ("sua cvc");
+import_pagmento ("confirmado");
+php;
